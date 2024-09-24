37b79adabf81288f234db782d17b52bc38ceef9a
rajat128 <155458603+rajat128@users.noreply.github.com>
2024-09-24 17:48:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,25 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { configDotenv } from 'dotenv';
 import theme from './components/theme.js';
-let domain = `${import.meta.env.REACT_APP_AUTH_DOMAIN}`;
-let clientId = `${import.meta.env.REACT_APP_CLIENTID}`;
+let domain = `${import.meta.env.VITE_APP_AUTH_DOMAIN}`;
+function check(){
+  console.log(domain);
+}
+
+let clientId = `${import.meta.env.VITE_APP_CLIENTID}`;
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
+ 
     <Auth0Provider
-    domain="dev-3v338n0cbkpdb41k.us.auth0.com"
-    clientId="4OMJ9pLy3BMAzFmTFm2J4N7yQUZWilLR"
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
     audience="https://dev-3v338n0cbkpdb41k.us.auth0.com/api/v2/"
   >
     <ChakraProvider theme={theme} >
+      {check()}
       <App />
       </ChakraProvider>
     </Auth0Provider>
